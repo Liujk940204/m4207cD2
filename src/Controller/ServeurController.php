098db4f9517fc48f0aad1ddd759e0d8617bcc188
liu <liujk940204@126.com>
2022-02-08 12:01:18 +0100

    utilisateur

--- src/Controller/ServeurController.php
+++ src/Controller/ServeurController.php
@@ -2,49 +2,65 @@
 
 namespace App\Controller;
 
+
+use App\Entity\NomClasseTable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class ServeurController extends AbstractController
 {
     /**
      * @Route("/serveur", name="serveur")
      */
-    public function index(Request $request): Response
+    public function index(): Response
     {   
-        $nom=$request->request->get ("nom");
-        $password=$request->request->get ("password");
-
-        if($password == "password"){
-            $contenu = "valide";
-        }
-        else{
-            $contenu = "non valide";
-        }
         
         return $this->render('serveur/index.html.twig', [
             'controller_name' => 'ServeurController',
-            'nom' =>$nom,
-            'password'=> $password,
         ]);
     }
 
 
     /**
-     * @Route("/login", name="login")
+     * @Route("/valide", name="valide")
      */
-    public function login(Request $request): Response
+    public function valide(Request $request): Response
     {
-    
+        $nom=$request->request->get ("admin");
+        $password=$request->request->get ("password");
         
-
-        return $this->render('serveur/login.html.twig', [
-            'controller_name' => 'ServeurController',
-            
-            
+        
+          if($password == "password" && $nom == "admin"){
+            $contenu = "valide";
+           }
+           else{
+            $contenu = "non valide";
+           }
+        
+        
+        return $this->render('serveur/valide.html.twig', [
+            'admin' =>$nom,
+            'contenu' => $contenu,
         ]);
-
     }
+
+    /**
+        * @Route("/login", name="login")
+        */
+        public function login(Request $request,EntityManagerInterface $manager): Response
+    {   
+        $reponse = $manager -> getRepository(NomClasseTable :: class) -> findOneBy([ 'nomChamp' => 'admin']);
+        $reponse -> getChamp();
+        
+        return $this->render('serveur/login.html.twig', [
+            
+
+        ]);
+    }
+
+
 }
